9f856abf07783ae08995e786493e4be8f7487f87
asaikali <asaikali@gmail.com>
2023-03-08 07:50:41 -0500

    polish

--- get-app-jar.sh
+++ get-app-jar.sh
@@ -1,6 +1,6 @@
 URL=https://maven.pkg.github.com/asaikali/demo-time/com/example/demo-time/${APP_VERSION}/demo-time-${APP_VERSION}.jar
 
-echo ${URL}
+echo "Downloading ${URL}"
 curl ${URL} \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   -L \
