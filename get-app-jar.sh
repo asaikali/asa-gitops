c9cdbc0378ddeb03869d3373fed173d0d7bda9ad
asaikali <asaikali@gmail.com>
2023-03-08 01:46:16 -0500

    update workflow

---
+++ get-app-jar.sh
@@ -0,0 +1,6 @@
+curl 'https://maven.pkg.github.com/asaikali/demo-time/com/example/demo-time/1.0.0/demo-time-1.0.0.jar' \
+  -H "Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}" \
+  -L \
+  -O
+
+  ls -lah
